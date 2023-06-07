36a4e65507c190d334e74c822d4a35ae60a28d51
unknown <hassanggg098@gmail.com>
2023-06-07 23:32:31 +0300

    release-5 relate #9

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
-// var currentDate = new Date();
-// var currentYear = currentDate.getFullYear();
-// var startDate = new Date(currentYear, 0, 1);
-// var endDate = new Date(currentYear, 11, 31);
-// var totalDays = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
-// var elapsedDays = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
-// var percentageRemaining = Math.floor(((totalDays - elapsedDays) / totalDays) * 100);
-
-// var progressBar = document.querySelector('.progress');
-// progressBar.style.width = percentageRemaining + "%";
-
-// document.getElementById("result").innerHTML = percentageRemaining + "%";
-
-// document.getElementById("info-year").childNodes[1].textContent = currentYear + "م";
-
-
-// Get the current date
-const currentDate = new Date();
-
-// Get the current year
-const currentYear = currentDate.getFullYear();
-
-// Set the target date to the last day of the current year
-const targetDate = new Date(currentYear, 11, 31); // Month is zero-based, so 11 represents December
-
-// Calculate the time difference in milliseconds
-const timeDifference = targetDate.getTime() - currentDate.getTime();
-
-// Convert the time difference to days
-const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-
-// Calculate the percentage of days remaining
-const daysPassed = 365 - daysRemaining;
-const percentageRemaining = Math.floor((daysRemaining / 365) * 100); // Round down to the nearest integer
-const percentagePassed = 100 - percentageRemaining;
-
-// Output the result
-var progressBar = document.querySelector('.progress');
-progressBar.style.width = percentagePassed + "%";
-
-
-document.getElementById("info-year").childNodes[1].textContent = currentYear + "م";
-document.getElementById("result").innerHTML = percentagePassed + "%";
-
+function calculateRemainingDaysAndWeeks() {
+    // Get the current date
+    const currentDate = new Date();
+  
+    // Get the current year
+    const currentYear = currentDate.getFullYear();
+  
+    // Set the target date to the last day of the current year
+    const targetDate = new Date(currentYear, 11, 31); // Month is zero-based, so 11 represents December
+  
+    // Calculate the time difference in milliseconds
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+  
+    // Convert the time difference to days
+    const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  
+    // Calculate the remaining weeks
+    const weeksRemaining = Math.ceil(daysRemaining / 7);
+  
+    // Calculate the percentage of days remaining
+    const daysPassed = 365 - daysRemaining;
+    const percentageRemaining = (daysRemaining / 365) * 100;
+    const percentagePassed = 100 - percentageRemaining;
+  
+    // Output the result
+    var progressBar = document.querySelector('.progress');
+    progressBar.style.width = percentageRemaining + "%";
+  
+    // Get the current date
+    const currentDateElement = new Date();
+  
+    // Format the date as "month/day/year"
+    const formattedDate = `${currentDateElement.getMonth() + 1}/${currentDateElement.getDate()}/${currentDateElement.getFullYear()}`;
+  
+    // Set the formatted date to the element
+    document.querySelector(".currentDate-element").textContent = formattedDate;
+  
+    // Update the remaining days and weeks
+    document.querySelector(".remaining-days").textContent = ` remaining days : ${daysRemaining}` ;
+    document.querySelector(".remaining-weeks").textContent = ` remaining weeks : ${weeksRemaining}`;
+  
+    document.getElementById("info-year").childNodes[1].textContent = currentYear + "م";
+    document.getElementById("result").innerHTML = percentagePassed.toFixed(0) + "%";
+  }
+  
+  // Call the function to calculate and display the remaining days and weeks
+  calculateRemainingDaysAndWeeks();
+  
