d0fb4fea98b89decb17f5aae68ca2ec27efcfa8e
unknown <hassanggg098@gmail.com>
2023-06-09 11:34:49 +0300

    release-15 relate #29

--- hijri/hijri.js
+++ hijri/hijri.js
@@ -1,30 +1,32 @@
 // Function to calculate the requested information
 function calculateHijriCalendar() {
   // Get the current Hijri date
-  const currentDate = moment().format('iDD/iMM/iYYYY');
+  const currentDate = moment().format('iDD/iMM');
+  const currentDateFull = moment().format('DD/MM/YYYY');
   const hijriYear = moment().format('iYYYY');
-  
+
   // Calculate the start and end dates of the current Hijri year
   const startOfYear = moment(hijriYear, 'iYYYY');
   const endOfYear = moment(hijriYear, 'iYYYY').add(1, 'iyear').subtract(1, 'days');
-  
+
   // Calculate the elapsed and remaining days
-  const elapsedDays = moment(currentDate, 'iDD/iMM/iYYYY').diff(startOfYear, 'days');
-  const remainingDays = endOfYear.diff(moment(currentDate, 'iDD/iMM/iYYYY'), 'days');
-  
+  const elapsedDays = moment(currentDateFull, 'DD/MM/YYYY').diff(startOfYear, 'days');
+  const remainingDays = endOfYear.diff(moment(currentDateFull, 'DD/MM/YYYY'), 'days');
+
   // Calculate the percentage of elapsed days
   const totalDaysOfYear = endOfYear.diff(startOfYear, 'days') + 1;
   const percentageElapsed = (elapsedDays / totalDaysOfYear) * 100;
-  
+
   // Calculate the number of weeks following the end of the Hijri year
   const weeksFollowingEnd = Math.ceil(remainingDays / 7);
-  
-  // Update the element with the current Hijri date
-  document.querySelector(".currentDate-element").textContent = currentDate;
-  
+
+  // Update the element with the current Hijri date in the format 'dd/mm'
+  const currentDateElement = document.querySelector(".currentDate-element");
+  currentDateElement.textContent = currentDate;
+
   // Update the year in the element with id "info-year"
   document.getElementById("info-year").childNodes[1].textContent = hijriYear;
-  
+
   // Return the calculated values
   return {
     elapsedDays,
@@ -46,10 +48,6 @@ document.getElementById("result").innerHTML = parseInt(hijriCalendarInfo.percent
 var progressBar = document.querySelector('.progress');
 progressBar.style.width = hijriCalendarInfo.percentageElapsed.toFixed(2) + '%';
 
-
-
-
-
 // Add click event listener to the copy button
 document.getElementById("copy-url-btn").addEventListener("click", function() {
   // Copy the URL to the clipboard
@@ -61,4 +59,3 @@ document.getElementById("copy-url-btn").addEventListener("click", function() {
   document.execCommand('copy');
   document.body.removeChild(el);
 });
-
